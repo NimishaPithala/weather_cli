3a897cd824567bf77d90df7a367ac3d26d1dfc05
Nimisha Pithala <76213967+NimishaPithala@users.noreply.github.com>
2025-09-10 16:11:43 +0200

    Add files via upload

---
+++ weather_cli.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/weather_cli.dir/main.cpp.o"
+  "CMakeFiles/weather_cli.dir/main.cpp.o.d"
+  "weather_cli"
+  "weather_cli.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/weather_cli.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
